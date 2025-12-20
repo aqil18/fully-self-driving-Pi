63c79095fa8e0da0819dd318255a0d8ae846bc29
Aqil F <aqilfaizal2004@gmail.com>
2025-12-20 01:00:53 +0000

    New test motor launch

---
+++ launch/test_motor_launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='self_driving_pkg',
+            namespace='test',
+            executable='image_test_node',
+            name='image_test'
+        ),
+        Node(
+            package='self_driving_pkg',
+            namespace='test',
+            executable='inference_node',
+            name='inference'
+        ),
+        Node(
+            package='self_driving_pkg',
+            namespace='test',
+            executable='motor_node',
+            name='motor'
+        )
+    ])
