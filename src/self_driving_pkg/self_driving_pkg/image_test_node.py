2e3b4d65f2c83fb021f13bba4cf2be5899a327bc
Aqil F <aqilfaizal2004@gmail.com>
2025-12-24 23:29:36 +0000

    updated topic

--- src/self_driving_pkg/self_driving_pkg/image_test_node.py
+++ src/self_driving_pkg/self_driving_pkg/image_test_node.py
@@ -51,7 +51,7 @@ def main(args=None):
     # home/ws on local machine
     image_directory = '/home/aqil/fully-self-driving-Pi/src/self_driving_pkg/self_driving_pkg/example_images'  # Change this path to your image folder
     
-    topic = '/camera/image_raw'
+    topic = '/fsd/image_raw'
     publish_rate = 1  # Hz
 
     image_publisher = ImagePublisher(image_directory, topic, publish_rate)
