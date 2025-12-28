5a668043a8e40787b049869624fb09247f5f34d1
Aqil F <aqilfaizal2004@gmail.com>
2025-12-28 12:35:29 -0500

    fl testing

--- src/self_driving_pkg/self_driving_pkg/car_utils/Motor_api.py
+++ src/self_driving_pkg/self_driving_pkg/car_utils/Motor_api.py
@@ -32,10 +32,10 @@ class Motor:
         #     FR = BR
         #     BR = temp
         
-        # self.pwm.setMotorPwm(0, max(0, FL))
-        # self.pwm.setMotorPwm(1, max(0, -FL))
-        self.pwm.setMotorPwm(2, max(0, BL))
-        self.pwm.setMotorPwm(3, max(0, -BL))
+        self.pwm.setMotorPwm(0, max(0, FL))
+        self.pwm.setMotorPwm(1, max(0, -FL))
+        # self.pwm.setMotorPwm(2, max(0, BL))
+        # self.pwm.setMotorPwm(3, max(0, -BL))
         # self.pwm.setMotorPwm(4, max(0, BR))
         # self.pwm.setMotorPwm(5, max(0, -BR))
         # self.pwm.setMotorPwm(6, max(0, FR))
