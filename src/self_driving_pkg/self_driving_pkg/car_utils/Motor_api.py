3ac55fc7c7a5650e916c6c62e6b97d9a821432cb
Aqil F <aqilfaizal2004@gmail.com>
2025-12-28 12:16:31 -0500

    fr

--- src/self_driving_pkg/self_driving_pkg/car_utils/Motor_api.py
+++ src/self_driving_pkg/self_driving_pkg/car_utils/Motor_api.py
@@ -34,10 +34,10 @@ class Motor:
         
         # self.pwm.setMotorPwm(0, max(0, FL))
         # self.pwm.setMotorPwm(1, max(0, -FL))
-        self.pwm.setMotorPwm(2, max(0, BL))
-        self.pwm.setMotorPwm(3, max(0, -BL))
-        # self.pwm.setMotorPwm(4, max(0, FR))
-        # self.pwm.setMotorPwm(5, max(0, -FR))
+        # self.pwm.setMotorPwm(2, max(0, BL))
+        # self.pwm.setMotorPwm(3, max(0, -BL))
+        self.pwm.setMotorPwm(4, max(0, FR))
+        self.pwm.setMotorPwm(5, max(0, -FR))
         # self.pwm.setMotorPwm(6, max(0, BR))
         # self.pwm.setMotorPwm(7, max(0, -BR))
 
