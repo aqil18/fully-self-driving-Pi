4e7689fce2b2770cf112c497e780a68c4c7223d6
Aqil F <aqilfaizal2004@gmail.com>
2025-12-27 22:58:11 -0500

    fixed setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /opt/ros/kilted/setup.bash
 colcon build --packages-select self_driving_pkg --symlink-install
-source install/setup.bash
+source ./install/setup.bash
 sudo chmod 666 /dev/i2c-1
