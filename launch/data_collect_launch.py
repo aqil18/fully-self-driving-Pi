94cb569df2268860a95f1a2dae8a3b56542abc0b
Aqil F <aqilfaizal2004@gmail.com>
2025-12-31 00:39:35 -0500

    adjusting the tele op node

---
+++ launch/data_collect_launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='v4l2_camera',
+            namespace='fsd',
+            executable='v4l2_camera_node',
+            name='camera'
+        ),
+        Node(
+            package='web_video_server',
+            namespace='fsd',
+            executable='web_video_server',
+            name='web_video_server'
+        ),
+        Node(
+            package='self_driving_pkg',
+            namespace='fsd',
+            executable='teleop_node',
+            name='teleop_node'
+        ),
+        Node(
+            package='self_driving_pkg',
+            namespace='fsd',
+            executable='motor_node',
+            name='motor'
+        )
+    ])
