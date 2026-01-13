1cf920709b559345c24033511b4052a7f8c42d8c
Aqil F <aqilfaizal2004@gmail.com>
2026-01-13 16:19:50 +0000

    removing datasets that may have persisted

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ colcon build --packages-select interfaces
 colcon build --packages-select self_driving_pkg --symlink-install
 source ./install/setup.bash
 sudo chmod 666 /dev/i2c-1
+rm -r ./launch/datasets/*
 echo "Setup complete."
