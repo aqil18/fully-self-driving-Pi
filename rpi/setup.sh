2bea521e283fb65fb8f8db5d631ab586d6609117
Aqil F <aqilfaizal2004@gmail.com>
2026-02-09 12:25:22 -0800

    trying to get rpi to use the env instead of system interpreter

--- rpi/setup.sh
+++ rpi/setup.sh
@@ -2,6 +2,7 @@
 source /opt/ros/kilted/setup.bash
 colcon build --packages-select interfaces
 colcon build --packages-select self_driving_pkg --symlink-install
+colcon build --packages-select web_video_server
 source ./install/setup.bash
 sudo chmod 666 /dev/i2c-1
 rm -r ./launch/datasets/*
