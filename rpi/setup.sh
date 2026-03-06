7776db3cfc50248e951de2839e52508374603a9d
Aqil F <aqilfaizal2004@gmail.com>
2026-03-05 17:08:41 -0800

    changes on setup

--- rpi/setup.sh
+++ rpi/setup.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-BOLDGREEN="\e[1;${GREEN}m"
-echo -e "\n${BOLDGREEN}Setting up ROS2 ${ENDCOLOR}"
+echo -e "\nSetting up ROS2"
 source /opt/ros/kilted/setup.bash
 
-echo -e "\n${BOLDGREEN}Building packages${ENDCOLOR}"
+echo -e "\nBuilding packages"
 colcon build --packages-select interfaces
 colcon build --packages-select self_driving_pkg --symlink-install
 colcon build --packages-select web_video_server
 source ./install/setup.bash
 
-echo -e "\m${BOLDGREEN}Adding permissions for i2C gateway${ENDCOLOR}"
+echo -e "\nAdding permissions for i2C gateway"
 sudo chmod 666 /dev/i2c-1
 
-echo -e "\n${BOLDGREEN}Removing previously recorded datasets${ENDCOLOR}"
+echo -e "\nRemoving previously recorded datasets"
 rm -r ./launch/datasets/*
 
-echo -e "\n${BOLDGREEN}Setting up Python venv${ENDCOLOR}"
+echo -e "\nSetting up Python venv"
 export PYTHONPATH="/home/aqil/fully-self-driving-Pi/envx/lib/python3.12/site-packages"
 source ../envx/bin/activate
 echo "\nSetup complete."
