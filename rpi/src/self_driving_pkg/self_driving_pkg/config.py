752aa316ab10e5d860de85c1e00fde18937b67c2
Aqil F <aqilfaizal2004@gmail.com>
2026-03-13 17:17:01 -0700

    hardcoded

--- rpi/src/self_driving_pkg/self_driving_pkg/config.py
+++ rpi/src/self_driving_pkg/self_driving_pkg/config.py
@@ -1,6 +1,4 @@
 from dataclasses import dataclass
-from ament_index_python.packages import get_package_share_directory
-import os
 
 
 # -----------------------------
@@ -9,9 +7,7 @@ import os
 @dataclass
 class Config:
     ### Model path
-    # Where your model weights are saved
-    pkg_share = get_package_share_directory('self_driving_pkg')
-    model_path = os.path.join(pkg_share, 'models', 'model.pt')
+    model_path = "/home/aqil/fully-self-driving-Pi/rpi/src/self_driving_pkg/self_driving_pkg/models/model.pt"
     
     ### Motor thresholds
     max_throttle = 40
