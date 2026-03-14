1e27aa3413b1c8ec15c7d1b05452877e2c00073b
Aqil F <aqilfaizal2004@gmail.com>
2026-03-13 17:06:38 -0700

    nn

--- rpi/src/self_driving_pkg/self_driving_pkg/config.py
+++ rpi/src/self_driving_pkg/self_driving_pkg/config.py
@@ -7,8 +7,8 @@ from dataclasses import dataclass
 class Config:
     ### Model path
     # Where your model weights are saved
-    model_path = '/models/model.pt' 
-
+    model_path = 'rpi/src/self_driving_pkg/self_driving_pkg/models/model.pt' 
+    
     ### Motor thresholds
     max_throttle = 40
     max_angle = 90 
